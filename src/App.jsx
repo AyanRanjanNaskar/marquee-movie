6b179bcb082d6410bb1a5a764745164bc062a293
Ayan Ranjan Naskar <ayanranjannaskar2004@gmail.com>
2025-12-23 14:02:42 +0530

    initialise + hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from 'react';
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div>App</div>
+    <main>
+      <div className='pattern'/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./images/favicon.png" alt="logo" />
+          <h1 className='self-edited'>Marquee Movies</h1>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h2>{searchTerm}</h2>
+      </div>
+    </main>
   )
 }
 
-export default App
+export default App 
