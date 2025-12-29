3efc2b3fdd10a38fce05ebef8955eea684f1acfd
Ayan Ranjan Naskar <ayanranjannaskar2004@gmail.com>
2025-12-29 12:51:36 +0530

    framer motion added + website intro

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,14 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MovieDetails from "./pages/MovieDetails";
+import { useState } from "react";
+import Intro from "./pages/Intro";
 
 const MainRouter = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/movie/:id" element={<MovieDetails />} />
-    </Routes>
-  );
+	const [introDone, setIntroDone] = useState(false);
+	return (
+		<>
+     {!introDone && <Intro onFinish={() => setIntroDone(true)} />}
+      {introDone && <Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/movie/:id" element={<MovieDetails />} />
+			</Routes>}
+			
+		</>
+	);
 };
 
 export default MainRouter;
